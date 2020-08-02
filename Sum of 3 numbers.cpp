e36684369030de4116d76d71dbd735a977d07590
DarshanS7 <69068659+DarshanS7@users.noreply.github.com>
2020-08-02 13:28:11 +0530

    Task on multiple inputs
    Sum of 3 nos.

---
+++ Sum of 3 numbers.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+int sum;
+int num1,num2,num3;
+printf("input 3 nos seperated by comma:");
+scanf("%d,%d,%d",&num1,&num2,&num3);
+sum=num1+num2+num3;
+printf ("sum of three nos:%d",sum);
+return(0);
+}
